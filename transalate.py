0633556066244f482201d92699bf53b9bf1a14aa
S-Kothandan <147807620+S-Kothandan@users.noreply.github.com>
2023-10-16 15:08:25 +0530

    Python translator app

---
+++ transalate.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
