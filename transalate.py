66e930a7349a24a7f05e689a63f20da90f6871b0
Saravana Kumar R <145033609+saravana-1042@users.noreply.github.com>
2023-10-18 10:37:49 +0530

    commit

--- transalate.py
+++ transalate.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/signup')
+# MongoDB configuration
+client = MongoClient('mongodb://localhost:27017/')
+db = client['MyDb']  
+collection = db['users']
+
+@app.route('/')
 def signup():
     return render_template('signup.html')
 
-@app.route('/')
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form['username']
+    password = request.form['password']
+    
+    existing_user = collection.find_one({'username': username})
+    
+    if existing_user:
+        return 'User already exists!'
+    else:
+     
+        collection.insert_one({'username': username, 'password': password})
+        return render_template('index.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
